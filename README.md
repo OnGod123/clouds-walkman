db10351e275c8b221849bab96dc45db3f24a9cb5
onGod123 <ifejikav4@gmail.com>
2023-03-26 19:56:05 -0700

    . I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

---
+++ README.md
@@ -0,0 +1,1 @@
+my collaborative task in Alx
--- main.h
+++ main.h
@@ -113,6 +113,7 @@ int is_digit(char);
 
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
+int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
 
